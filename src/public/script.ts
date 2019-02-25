f6507829ff1d87ba0b5bd4b83e51e67c4df00edd
Salif Mehmed <salif@programmer.net>
2019-02-25 19:35:08 +0200

    fix bugs and continue developing

--- src/public/script.ts
+++ src/public/script.ts
@@ -1,37 +1,64 @@
-window.addEventListener("error", (err) => {
-    alert("Something went wrong! " + err.message);
-});
+import { join } from "path";
 
-let _evalCode_orig_code: any[] = [];
-let _evalCode_orig_args: any[] = [];
-let _evalCode_orig_xml: any;
+let _input: any[] = [];
+let _output: any[] = [];
+let _xml: any;
+let _isRunning: boolean = false;
+let _console: any[][] = [[], [], [], [], []];
+
 (() => {
-    let _evalCode_con: any[] = [];
-    function js(code: string, args: any[]): void {
-        ((_evalCode_code) => {
+    window.addEventListener("load", () => {
+        let exeFile: XMLHttpRequest = new XMLHttpRequest();
+        exeFile.open("GET", "exercises.xml", true);
+        exeFile.onreadystatechange = function () {
+            if (exeFile.readyState === 4 && (exeFile.status === 200 || exeFile.status == 0)) {
+                let exedEl: any = document.getElementById("exed");
+                let xmlc: any = exeFile.responseXML.firstChild;
+                let exe1d: any = xmlc.getElementsByTagName("exe1")[0];
+                exedEl.innerHTML = "<h3>Exercise 1: " +
+                    exe1d.getElementsByTagName("title")[0]
+                        .firstChild.nodeValue.trim() + "</h3><pre>" +
+                    exe1d.getElementsByTagName("description")[0]
+                        .firstChild.nodeValue.trim() + "</pre>";
+                _xml = xmlc;
+            }
+        }
+        exeFile.send();
+    });
+
+    window.addEventListener("error", (err) => {
+        alert("Something went wrong! " + err.message);
+    });
+
+    function _run_js(code: string, args: any, ci:number): void {
+        ((code: string) => {
             let console = {
                 log: (v: any) => {
-                    _evalCode_con.push(v);
+                    _console[ci].push(v);
                 },
                 error: (v: any) => {
-                    _evalCode_con.push(v);
+                    _console[ci].push(v);
                 },
                 warn: (v: any) => {
-                    _evalCode_con.push(v);
+                    _console[ci].push(v);
                 }
             };
-            const _evalCode_args: any[] = _evalCode_orig_args;
-            let _evalCode_i: number = -1;
+            let _i: number = -1;
             let alert: Function = (v: any) => {
                 console.log(v);
             };
             let prompt: Function = (v: any) => {
-                return _evalCode_args[++_evalCode_i];
+                return args[++_i];
             };
-            let confirm: Function = () => {};
-            let _evalCode_submit: Function = () => { };
-            let _evalCode_orig_code: string = "";
-            let _evalCode_exeChange = null;
+            let confirm: Function = () => { };
+            let _submit:any=null;
+            let _run_code:any=null;
+            let _exeChange = null;
+            let _run_js:any=null;
+            let _input:any=null;
+            let _output:any=null;
+            let _xml:any = null;
+            let _isRunning:any=null;
             let window: any = null;
             let document: any = null;
             let XMLHttpRequest: any = null;
@@ -41,76 +68,84 @@ let _evalCode_orig_xml: any;
             let screen: any = null;
             let location: any = null;
             let history: any = null;
-            let navigator:any=null;
-            let setInterva:any=null;
-            let setTimeout:any=null;
-            let cookie:any=null;
-            eval(_evalCode_code);
+            let navigator: any = null;
+            let setInterval: any = null;
+            let setTimeout: any = null;
+            let cookie: any = null;
+            eval(code);
         }).call({}, code);
     }
-    function _evalCode_submit(): void {
-        const codeEl: any = document.getElementById("code");
-        let code: string = codeEl.value;
-        const langEl: any = document.getElementById("lang");
-        let lang: string = langEl.value;
-        const exeEl: any = document.getElementById("exe");
-        let exe: string = exeEl.value;
-        switch (exe) {
-            case "exe1":
-                let exejs = window["exercises"]["exe1"];
-                _evalCode_orig_args = exejs[1];
-                _evalCode_orig_code = [exejs[2], exejs[3], exejs[4], exejs[5], exejs[6]];
-                break;
+
+    function _run_code(lang: string, code: string, args: any[]) {
+        for (let i = 0; i < args.length; i++) {
+            _run_js(code, args[i], i);
         }
-        switch (lang) {
-            case "js":
-                for (let i = 0; i < 5; i++) {
-                    js(code, _evalCode_orig_args);
-                }
-                break;
+    }
+
+    function _submit(): void {
+        if (_isRunning) {
+            alert("Please wait!");
+            return;
         }
+        _isRunning = true;
+        const codeElement: any = document.getElementById("code");
+        let code: string = codeElement.value;
+        const langElement: any = document.getElementById("lang");
+        let lang: string = langElement.value;
+        const exeElement: any = document.getElementById("exe");
+        let exeNum: string = exeElement.value;
+        let xml: any = _xml.getElementsByTagName("exe" + exeNum)[0];
+        _input = [
+            xml
+                .getElementsByTagName("a1")[0]
+                .getElementsByTagName("input")[0]
+                .firstChild.nodeValue.trim(),
+            xml
+                .getElementsByTagName("a2")[0]
+                .getElementsByTagName("input")[0]
+                .firstChild.nodeValue.trim(),
+            xml
+                .getElementsByTagName("a3")[0]
+                .getElementsByTagName("input")[0]
+                .firstChild.nodeValue.trim(),
+            xml
+                .getElementsByTagName("a4")[0]
+                .getElementsByTagName("input")[0]
+                .firstChild.nodeValue.trim(),
+            xml
+                .getElementsByTagName("a5")[0]
+                .getElementsByTagName("input")[0]
+                .firstChild.nodeValue.trim(),
+        ];
+        _run_code(lang, code, _input);
         setTimeout(() => {
-            if (_evalCode_con.join("\n") === _evalCode_orig_code.join()) {
+            alert("console: " + JSON.stringify(_console));
+            alert("output: " + JSON.stringify(_output));
+            if (_console.join("\n") === _output.join()) {
                 alert(true);
             }
             else {
-                alert(_evalCode_con);
+                alert(_console);
             }
-            _evalCode_con = [];
+            _console = [[], [], [], [], []];
+            _isRunning = false;
         }, 1000)
     }
-    window["_evalCode_submit"] = _evalCode_submit;
-    function _evalCode_exeChange(): void {
+    window["_submit"] = _submit;
+
+    function _exeChange(): void {
         let exeNum: string = "";
         const exeEl: any = document.getElementById("exe");
         exeNum = exeEl.value;
         document.getElementById("exed").innerHTML = "<h3>Exercise " + exeNum + ": " +
-            _evalCode_orig_xml
+            _xml
                 .getElementsByTagName("exe" + exeNum)[0]
                 .getElementsByTagName("title")[0]
                 .firstChild.nodeValue.trim() + "</h3><pre>" +
-            _evalCode_orig_xml
+            _xml
                 .getElementsByTagName("exe" + exeNum)[0]
                 .getElementsByTagName("description")[0]
                 .firstChild.nodeValue.trim() + "</pre>"
     }
-    window["_evalCode_exeChange"] = _evalCode_exeChange;
+    window["_exeChange"] = _exeChange;
 })();
-window.addEventListener("load", () => {
-    let exeFile: XMLHttpRequest = new XMLHttpRequest();
-    exeFile.open("GET", "exercises.xml", true);
-    exeFile.onreadystatechange = function () {
-        if (exeFile.readyState === 4 && (exeFile.status === 200 || exeFile.status == 0)) {
-            let exedEl: any = document.getElementById("exed");
-            let xmlc: any = exeFile.responseXML.firstChild;
-            let exe1d: any = xmlc.getElementsByTagName("exe1")[0];
-            exedEl.innerHTML = "<h3>Exercise 1: " +
-                exe1d.getElementsByTagName("title")[0]
-                    .firstChild.nodeValue.trim() + "</h3><pre>" +
-                exe1d.getElementsByTagName("description")[0]
-                    .firstChild.nodeValue.trim() + "</pre>";
-            _evalCode_orig_xml = xmlc;
-        }
-    }
-    exeFile.send();
-});
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-const express = require('express')
-const app = express()
+const express: any = require('express');
+const app: any = express();
+const path: any = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req: any, res: any) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.listen(3000, () => console.log('Listening on port 3000!'));
